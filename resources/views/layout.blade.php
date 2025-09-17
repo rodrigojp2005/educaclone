11dbde6b8b69ea76e8759cac27b50e5bc764ba0e
rodrigojp2005 <rodrigojp2005@gmail.com>
2025-09-17 16:05:15 -0300

    publicação de main master

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -170,7 +170,7 @@
         <div class="container">
             <a class="navbar-brand" href="{{ route('home') }}">
                 <i class="fas fa-graduation-cap me-2"></i>
-                TutoCursos <span class="fst-italic" style="font-weight: normal; font-size: 1rem;">tutoriais e cursos</span>
+                TutoCursos <span class="fst-italic" style="font-weight: normal; font-size: 1rem;">tutoriais e cursos!</span>
             </a>
 
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileNav" aria-controls="mobileNav" aria-label="Abrir menu">
