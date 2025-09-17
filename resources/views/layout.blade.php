ac574f2452fb80c5b92869b9e6f947f742675eef
rodrigojp2005 <rodrigojp2005@gmail.com>
2025-09-17 15:41:56 -0300

    layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -170,7 +170,7 @@
         <div class="container">
             <a class="navbar-brand" href="{{ route('home') }}">
                 <i class="fas fa-graduation-cap me-2"></i>
-                EduClone
+                TutoCursos
             </a>
 
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileNav" aria-controls="mobileNav" aria-label="Abrir menu">
