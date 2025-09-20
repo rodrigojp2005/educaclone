4c31d56da756fb22503ef9829497c5017d58a125
rodrigojp2005 <rodrigojp2005@gmail.com>
2025-09-20 13:32:54 -0300

    :

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,7 +10,7 @@
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                            Total de Usuários
+                            Total de Usuários:
                         </div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">{{ number_format($stats['total_users']) }}</div>
                     </div>
